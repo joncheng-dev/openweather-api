cb327f78100928eb2bd72b8eee174589a4031766
Jonathan Cheng <jcheng461@gmail.com>
2022-07-08 12:04:50 -0700

    set up template to work for open weather app. User enters a city. API call is made, returning humidity and temperature data for user specified city.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../css/styles.css";
+import "./css/styles.css";
 
 // User Interface Logic
 $(document).ready(function () {
-  // Add here
+  // Add
+  $("#weatherLocation").click(function () {
+    const city = $("#location").val();
+    $("#location").val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $(".showHumidity").text(
+        `The humidity in ${city} is ${response.main.humidity}%`
+      );
+      $(".showTemp").text(
+        `The temperature in Kelvins is ${response.main.temp} degrees.`
+      );
+    }
+  });
 });
